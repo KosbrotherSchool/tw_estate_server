e4f10bd0e9b5418148931d7093c5929597e4b45f
KosbrotherSchool <kosbrotherschool@gmail.com>
2013-12-17 11:31:22 +0800

    add get_estate_item method

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ TwEatateServer::Application.routes.draw do
       resources :real_estate, :only => [:index, :show] do
         collection do
           get 'around_all_by_areas'
-          get 'around_all'
-          get 'estate_around'
-          get 'presale_around'
-          get 'rent_around'
-          get 'estate_and_pre_sale_around'
-          get 'estate_and_rent_around'
-          get 'pre_sale_and_rent_around'
+          get 'get_estate_item'
+          # get 'around_all'
+          # get 'estate_around'
+          # get 'presale_around'
+          # get 'rent_around'
+          # get 'estate_and_pre_sale_around'
+          # get 'estate_and_rent_around'
+          # get 'pre_sale_and_rent_around'
         end
       end
     end
